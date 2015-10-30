303164c9074eaf0d2c6a0333a5982b8dde1e645f
Balm Ann <higgs.mel2433@student.swatc.edu>
2015-10-30 09:18:49 -0600

    Create monster.java

---
+++ monster.java
@@ -0,0 +1,44 @@
+
+public class monster {
+	public static String name;
+	public static int hp;
+	public static int attack;
+	public static int defense;
+	public static int res;
+	public static int drop;
+	public static String weak;
+public static void WildWolf(){
+	name = "WildWolf";
+	monster.hp = 15;
+	monster.attack = 11;
+	monster.defense = 10;
+	monster.res = 8;
+	monster.drop = 12;
+	monster.weak = "Fire";
+}
+public static void WildBear(){
+	name = "WildBear";
+	monster.hp = 17;
+	monster.attack = 17;
+	monster.defense = 12;
+	monster.drop = 15;
+	monster.weak = "Fire";
+}
+public static void Floor1Boss(){
+	name = "Dino";
+	monster.hp = 15;
+	monster.attack = 11;
+	monster.defense = 10;
+	monster.drop = 25;
+	monster.weak = "Fire";
+}
+public static void randomEncounter(){
+	final int HIGH = 10;
+	final int LOW = 1;
+	int random = ((int)(Math.random() * 100) % HIGH + LOW);
+	if (status.level < random)
+		WildWolf();
+	else
+		WildBear();
+}
+}
