834796cb718e5376414e5d4f7bf049a1a74e9bae
Balm Ann <higgs.mel2433@student.swatc.edu>
2015-10-31 20:12:19 -0600

    Update monster.java

--- monster.java
+++ monster.java
@@ -5,6 +5,8 @@ public class monster {
 	public static int attack;
 	public static int defense;
 	public static int res;
+	public static int spd;
+	public static int hit;
 	public static int drop;
 	public static String weak;
 public static void WildWolf(){
@@ -13,6 +15,8 @@ public static void WildWolf(){
 	monster.attack = 11;
 	monster.defense = 10;
 	monster.res = 8;
+	monster.spd= 10;
+	monster.hit = 4;
 	monster.drop = 12;
 	monster.weak = "Fire";
 }
@@ -22,6 +26,8 @@ public static void WildBear(){
 	monster.attack = 17;
 	monster.defense = 12;
 	monster.res = 9;
+	monster.spd=7;
+	monster.hit = 7;
 	monster.drop = 15;
 	monster.weak = "Fire";
 }
@@ -31,6 +37,8 @@ public static void Floor1Boss(){
 	monster.attack = 11;
 	monster.defense = 10;
 	monster.res=10;
+	monster.spd = 4;
+	monster.hit = 10;
 	monster.drop = 25;
 	monster.weak = "Fire";
 }
